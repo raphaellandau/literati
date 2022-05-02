864855d5244d2ec7cf9d9cd3c376d1ab818dc0ee
gg-eagleray <75076612+gg-eagleray@users.noreply.github.com>
2022-05-02 15:38:14 +0200

    recent developments and css fixes

--- wp-content/themes/hello-theme-child-master/functions.php
+++ wp-content/themes/hello-theme-child-master/functions.php
@@ -1,4 +1,12 @@
-<?php 
+<?php
+add_action ('parse_query', function() { 
+    if(is_admin()) return true;
+    if(is_page('on-boarding') && is_user_logged_in()) {
+         wp_redirect('/logged-in-user/');
+         die();
+    }
+});
+
 
 function hello_elementor_child_enqueue_scripts() {
 	wp_enqueue_style(
@@ -7,7 +15,7 @@ function hello_elementor_child_enqueue_scripts() {
 		[
 			'hello-elementor'
 		],
-		'1.0.0'
+		'1.0.14'
 	);
 }
 add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts' );
@@ -162,3 +170,95 @@ function array_values_recursive( $array ) {
     }
     return $array;
 }
+
+add_filter ( 'sanitize_user', 'generate_user_from_email', 10, 3);
+
+function generate_user_from_email($username, $raw_username, $strict ) {
+   return str_replace(' ', '_', $raw_username);
+    
+}
+
+add_filter('the_content', 'populate_fields', 999);
+
+function populate_fields($content) {
+    $content = str_replace('Array', '', $content);
+    if(is_user_logged_in()) {
+        $user_meta = get_user_meta(get_current_user_id());
+        if(isset($user_meta['facebook_gender']) && is_array($user_meta['facebook_gender'])) {
+            $gender = $user_meta['facebook_gender'][0];
+            if($gender != 'female') {
+                $content = str_replace("option value='אישה' selected='selected'", "option value='אישה'", $content);
+                if ($gender == 'male') {
+                    $content = str_replace("option value='גבר'", "option value='גבר' selected='selected'", $content);
+                } else {
+                    $content = str_replace("option value='אחר'", "option value='אחר' selected='selected'", $content);
+                }
+            }
+        }
+        if(isset($user_meta['facebook_gender']) && is_array($user_meta["google_birthdays"])) {
+            $birthday = $user_meta["google_birthdays"][0];
+            
+            if(isset($_GET['test'])) {
+            $google_date = date_create_from_format('Y-m-d', $birthday);
+            if($google_date) {
+                $now = date_create('now');
+                $diff = (date_diff($now, $google_date));
+                $age = $diff->y; 
+                if($age > 60) {
+                    $content = str_replace(" value='מעל 60'  ", " value='מעל 60'  checked ", $content);
+                } else if($age > 50) {
+                    $content = str_replace(" value='50-60'  ", " value='50-60'  checked ", $content);
+                } else if($age > 40) {
+                    $content = str_replace(" value='40-50'  ", " value='40-50'  checked ", $content);
+                } else if($age > 30) {
+                    $content = str_replace(" value='30-40'  ", " value='30-40'  checked ", $content);
+                } else if($age > 20) {
+                    $content = str_replace(" value='20-30'  ", " value='20-30'  checked ", $content);
+                } else {
+                    $content = str_replace(" value='מתחת ל-20'  ", " value='מתחת ל-20'  checked ", $content);
+                } 
+            }
+
+            }
+        }
+
+        if(isset($user_meta['facebook_gender']) && is_array($user_meta["facebook_birthday"])) {
+            $birthday = $user_meta["facebook_birthday"][0];
+            if(isset($_GET['test'])) {
+            $facebook_date = date_create_from_format('m/d/Y', $birthday);
+            if($facebook_date) {
+                $now = date_create('now');
+                $diff = (date_diff($now, $facebook_date));
+                $age = $diff->y; 
+                if($age < 20) {
+                    $content = str_replace(" value='מתחת ל-20'  ", " value='מתחת ל-20'  checked ", $content);
+                } else if($age < 30) {
+                    $content = str_replace(" value='20-30'  ", " value='20-30'  checked ", $content);
+                } else if($age < 40) {
+                    $content = str_replace(" value='30-40'  ", " value='30-40'  checked ", $content);
+                } else if($age < 50) {
+                    $content = str_replace(" value='40-50'  ", " value='40-50'  checked ", $content);
+                } else if($age < 60) {
+                    $content = str_replace(" value='50-60'  ", " value='50-60'  checked ", $content);
+                } else {
+                    $content = str_replace(" value='מעל 60'  ", " value='מעל 60'  checked ", $content);
+                } 
+            }
+
+            }
+        }
+
+
+    }
+    
+    
+    $likes_group = get_page_by_title('Likes and Dislikes', OBJECT, 'wp-types-user-group' );
+    //echo('reading fields...');
+    //var_dump($likes_group->ID);
+    //$meta = get_post_meta($likes_group->ID);
+    //var_dump($meta);
+    $data = get_option('wpcf-usermeta');
+    //var_dump($data); 
+    return $content;
+}
+
